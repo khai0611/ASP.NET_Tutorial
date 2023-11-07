edea628c834e48e0f579e31de42cefa2f13af6cc
hqk0611 <khaihq19@uef.edu.vn>
2023-11-07 11:14:14 +0700

    Add project files.

---
+++ ASP.NET Tutorial/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using ASP.NET_Tutorial.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASP.NET_Tutorial.Controllers
+{
+    //
+    public class HomeController : Controller
+    {
+        //
+        // GET: Home
+
+        public ActionResult Index()
+        {
+            //Pass data from controller to view: use Model and ViewBag
+            ViewBag.WelcomeString = "Welcome to ViewBag";
+
+            var message = new MessageModel(); // create a obj
+            message.Welcome = "Welcome to Model"; 
+            return View(message);
+        }
+    }
+}
+
+// Note: In ASP.NET MVC, name of Controller must have Controller name at the end
+// In controller, have methods that return to Result call is ActionMethod
---
+++ ASP.NET Tutorial/Models/MessageModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASP.NET_Tutorial.Models
+{
+    public class MessageModel
+    {
+        public string Welcome { get; set; }
+    }
+}
